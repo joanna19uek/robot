18394702fcb5b90bbdacefebf44ca9bd9ff87d39
Lukasz Pudzisz <Lukasz.Pudzisz.@gmail.com>
2016-11-18 02:09:01 +0100

    Add new colors and countent

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 		<title>T-1000-(4Melmak)</title>
 		<meta charset="UTF-8">
 		<link href="style.css"  rel="stylesheet" type="text/css">
-		<link rel="Shortcut icon" href="http://v-ie.uek.krakow.pl/~s187924/pp5/robotlogoook1.jpg" />
+		<link rel="Shortcut icon" href="http://v-ie.uek.krakow.pl/~s187924/pp5/robot/logoook1.jpg" />
 	<head>
 <body>
-<div>
+
 	<div class="gora">
 	<h2 id="slogan">Nasze odkurzacze sa THE BEST - poodkurzasz nimi FEST!</h2>
 	</div>
@@ -17,24 +17,25 @@
 </div>
 
 	<div class="menu">
-	<a href="produkt.php">Dane Techniczne</a><br>
-	<a href="strona.php">Kontakt</a><br>
-	<a href="galeria.php">Galeria</a>
-
+	<ul>
+	<li><a href="produkt.php">Dane Techniczne</a>
+	<li><a href="strona.php">Kontakt</a>
+	<li><a href="galeria.php">Galeria</a>
+	</ul>
 
 	</div>
 
-<div class="ods">
-</div>
+
+
 
 	<div class="srodek">
-	<iframe width="560" height="315" src="https://www.youtube.com/embed/zs1uW2UMzOc" frameborder="0" allowfullscreen></iframe>
-	</div>
 <br>
-
-
-
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/zs1uW2UMzOc" frameborder="0" allowfullscreen></iframe>
+<br>	
+<br>
 </div>
+
+
 <div class="stopka">
 <h3>ACME Company</h3>
 </div>
